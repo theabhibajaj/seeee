b17712178a8bda8e7286d04097aefea50fdf4b36
theabhibajaj <89036139+theabhibajaj@users.noreply.github.com>
2023-01-18 22:09:38 -0500

    Add files via upload

---
+++ see/book.js
@@ -0,0 +1,43 @@
+img = "";
+status = "";
+objects = [];
+function leave(){
+    window.location = "index.html";
+}
+
+function preload(){
+img = loadImage("book.jpg");
+}
+function setUp(){
+    canvas = createCanvas(640,  420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting objects"
+    }
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+if(error){
+    console.log(error);
+}
+console.log(results);
+objects = results;
+}
+
+function setUp(){
+canvas = createCanvas(640,  420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status: Detecting objects"
+}
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status != "")
+    {
+    }
+    
+}
---
+++ see/main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+function tv(){
+    window.location = "tv.html";
+}
+function desk(){
+    window.location = "desk.html";
+}
+function bottles(){
+    window.location = "bottle.html";
+}
+function book(){
+    window.location = "book.html";
+}
